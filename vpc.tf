4c2fa4d70f2b9c0668fa471ce2433cd320f09155
joao neto <joaonetotfm@gmail.com>
2024-01-12 21:01:29 -0300

    refatoracao criacao subnets

--- vpc.tf
+++ vpc.tf
@@ -1,26 +1,18 @@
 resource "aws_vpc" "minha_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "VPC-fc"
+    Name = "${var.prefix}-VPC-fc"
   }
 }
 
 data "aws_availability_zones" "available" {}
-
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.minha_vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "subnets" {
+  count = 2
+  availability_zone =  data.aws_availability_zones.available.names[count.index]
+  vpc_id = aws_vpc.minha_vpc.id
+  cidr_block = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
   tags = {
-    Name = "Main"
-  }
-}
-
-resource "aws_subnet" "subnet2" {
-  vpc_id     = aws_vpc.minha_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "subnet2"
+    Name = "${var.prefix}-subnet-${count.index}"
   }
 }
